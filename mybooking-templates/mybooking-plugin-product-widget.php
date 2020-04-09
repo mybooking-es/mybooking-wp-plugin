1eacf08c3931990d8a657ead727e68b91f8aa903
Juan Gil Miqueo <jgil@MacBook-Pro-de-Juan.local>
2020-04-09 17:36:33 +0200

    product widget : single date

--- mybooking-templates/mybooking-plugin-product-widget.php
+++ mybooking-templates/mybooking-plugin-product-widget.php
@@ -1,6 +1,6 @@
     <div id="product_selector" data-code="<?php echo $args['code']?>" class="container is-desktop">
       <div class="row">
-        <div class="col-xs-12">
+        <div class="col-12">
           <form
             name="search_form"
             method="get"
