c92d6cce05c79debfcb4eb39dc7a1b7f526cf787
Juan Gil Miqueo <jgil@MacBook-Pro-de-Juan.local>
2020-06-08 12:13:11 +0200

    products caracteristics

--- mybooking-templates/mybooking-plugin-products.php
+++ mybooking-templates/mybooking-plugin-products.php
@@ -3,14 +3,42 @@
 		<div class="row">
 			<?php foreach( $args['data']->data as $product ) { ?>
 			  <div class="col-md-4">
-					<div class="resource-product-card card d-flex flex-column mb-2">
-					  <img class="resource-product-card-img card-img-top" src="<?php echo $product->photo_path?>" alt="?php echo $product->name?>">
+					<div class="resource-product-card card d-flex flex-column mb-2 shadow mb-5 bg-white rounded-0">
+					  <img class="resource-product-card-img card-img-top rounded-0" src="<?php echo $product->photo_path?>" alt="?php echo $product->name?>">
 					  <div class="card-body d-flex flex-column justify-content-center">
-					    <h5 class="h6 card-title resource-product-card-title"><?php echo $product->name ?></h5>
-					    <p class="card-text text-center text-muted"><?php echo $product->short_description ?></p>
+					    <h5 class="h6 card-title text-left resource-product-card-title"><b><?php echo $product->name ?></b></h5>
+					    <p class="card-text text-muted"><?php echo $product->short_description ?></p>
+			            <!-- From price -->
+			            <?php if ($product->from_price > 0) { ?>
+			            <p class="card-text text-danger font-weight-normal"><?php printf( _x('From <b>%s</b>', 'renting_products', 'mybooking-wp-plugin' ), number_format_i18n( $product->from_price ) ) ?>€</p>
+			        	<?php } else { ?>
+			        	<br>	
+			            <?php } ?>
 					  </div>
+				      <?php if ( isset( $product->key_characteristics) && is_array( (array) $product->key_characteristics ) && !empty( (array) $product->key_characteristics ) ) { ?>
+					  <div class="card-body key-characteristics">
+						<ul class="icon-list-items">
+			                <?php if ( isset( $product->characteristic_length ) && !empty( $product->characteristic_length ) ) { ?>
+			                  <li class="icon-list-item">
+			                      <span class="icon-list-icon">
+			                      	<img src="<?php echo plugin_dir_url(__DIR__) ?>/assets/images/characteristics/length.svg"/>
+			                      </span>
+			                      <span class="icon-list-text text-muted"><?php printf( _x('<b>%.2f</b> m.', 'renting_products', 'mybooking-wp-plugin' ), $product->characteristic_length ) ?></span>
+			                  </li>
+			                <?php } ?>  							
+							<?php foreach ( $product->key_characteristics as $key => $value) { ?>
+							<li class="icon-list-item">
+									<span class="icon-list-icon">
+										<img src="<?php echo plugin_dir_url(__DIR__) ?>/assets/images/key_characteristics/<?php echo $key ?>.svg"/>
+									</span>
+									<span class="icon-list-text text-muted"><?php echo $value ?></span>
+							</li>
+							<?php } ?>
+						</ul>
+					  </div>	
+			          <?php } ?>
 					  <div class="card-body d-flex flex-column justify-content-end">
-					    <a href="/<?php echo $args['url_detail']?>/<?php echo $product->code?>" class="btn btn-primary w-100">Más información</a>
+					    <a href="/<?php echo $args['url_detail']?>/<?php echo $product->code?>" class="btn btn-primary w-100"><?php echo _x('More information', 'renting_products', 'mybooking-wp-plugin' ) ?></a>
 					  </div>
 					</div>
 			  </div>
@@ -24,7 +52,7 @@
 				<nav aria-label="Page navigation example" class="pull-right">
 				  <ul class="pagination">
 				    <li class="page-item <?php if ($args['current_page'] == 1) { ?>disabled<?php } ?>">
-				    	  <a class="page-link" href="/<?php echo $args['url']?>?offsetpage=<?php echo $args['current_page']-1 ?>">Previous</a>
+				    	  <a class="page-link" href="/<?php echo $args['url']?>?offsetpage=<?php echo $args['current_page']-1 ?>"><?php _x('Previous', 'renting_products', 'mybooking-wp-plugin' ) ?></a>
 				    </li>
 	          <?php foreach ($args['pages'] as $page) { ?>
 		          <?php if ($page == $args['current_page']) { ?>
@@ -40,7 +68,7 @@
 		      	  <?php } ?>
 				    <?php } ?>	    
 				    <li class="page-item <?php if ($args['current_page'] == $args['total_pages']) { ?>disabled<?php } ?>">
-				    	  <a class="page-link" href="/<?php echo $args['url']?>?offsetpage=<?php echo $args['current_page']+1 ?>">Next</a>
+				    	  <a class="page-link" href="/<?php echo $args['url']?>?offsetpage=<?php echo $args['current_page']+1 ?>"><?php _x('Next', 'renting_products', 'mybooking-wp-plugin' ) ?></a>
 				    </li>
 				  </ul>
 				</nav>
--- mybooking-templates/mybooking-plugin-routes-product.php
+++ mybooking-templates/mybooking-plugin-routes-product.php
@@ -1,86 +1,94 @@
 <?php get_header();?>
 <br>
-<div class="container">
-	<div class="row">
+<div class="container product-container">
+  <div class="row">
     <div class="col-md-8">
-			<div class="shadow mb-5 bg-white rounded">
-	    	<!-- The photo -->
-	    	<?php if (!empty( $args->photos ) && count( $args->photos ) > 1) { ?>
-				<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-				  <div class="carousel-inner">
-				  	<?php foreach( $args->photos as $key => $photo ) { ?>
-				    <div class="carousel-item <?php if ($key == key($args->photos)) { ?>active<?php } ?>">
-				      <img class="d-block w-100" src="<?php echo $photo->full_photo_path?>" alt="<?php echo $args->name?>">
-				    </div>
-				    <?php } ?>
-				  </div>
-				  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-				    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-				    <span class="sr-only"><? echo _x('Previous', 'renting_product_detail', 'mybooking-wp-plugin' ) ?></span>
-				  </a>
-				  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-				    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-				    <span class="sr-only"><? echo _x('Next', 'renting_product_detail', 'mybooking-wp-plugin' ) ?></span>
-				  </a>
-				</div>
-				<?php } else if (count($args->photos) == 1) { ?>
-	  			<img class="d-block product-photo" src="<?php echo $args->photos[0]->full_photo_path?>" alt="<?php echo $args->name?>">
-				<?php } else { ?>
-				    <div class="text-center no-product-photo pt-3"><i class="fa fa-camera" aria-hidden="true"></i></div>
-				<?php } ?>
-				<div class="p-3">
-					<!-- Name -->
-					<h1 class="h2 mt-3"><?php echo $args->name ?></h1>
-					<!-- Short description -->
-		      <?php if ( !empty( $args->short_description) ) { ?>
-		        <div class="text-muted mt-1">
-		        	<?php echo $args->short_description ?>
-		        </div>
-		      <?php } ?>
-					<!-- From price -->
-		      <?php if ($args->from_price > 0) { ?>
-		      <h2 class="h3 mt-3 text-danger font-weight-normal"><? printf( _x('From <b>%s</b>', 'renting_product_detail', 'mybooking-wp-plugin' ), $args->from_price ) ?></h2>
-		      <?php } ?>
-		      <?php if ( isset( $args->characteristic_length ) && !empty( $args->characteristic_length ) ) { ?>
-		      <div class="mt-4">
-		      	<span class="fa fa-arrows-h"><?php echo $arg->characteristic_length ?></span>
-		      </div>
-		      <? } ?> 
-		      <!-- Key characteristics -->
-		      <?php if ( isset( $args->key_characteristics) && !empty( $args->key_characteristics ) ) { ?>
-		        <div class="product-card-characteristics mt-4">
-		      	<? foreach ( $args->key_characteristics as $key => $value) { ?>
-		      		<div class="icon">
-                <img src="<?php echo plugin_dir_url(__DIR__) ?>/assets/images/key_characteristics/<?php echo $key ?>.svg"/>
-                <span><?php echo $value ?></span>
-		      		</div>	
-		      	<? } ?>
-		      	</div>
-		      <?php } ?>
-		      <!-- Description -->
-		      <?php if ( !empty( $args->description ) ) { ?>
-		        <hr>
-			      <div class="mt-3">
-			      	<h2 class="h3 mb-3"><? echo _x('Details', 'renting_product_detail', 'mybooking-wp-plugin' ) ?></h2>
-			      	<?php echo $args->description ?>
-			      </div>
-		      <?php } ?>
-		    </div>
-    	</div>
-		</div>
-		<div class="col-md-4">
-			<div class="container">
-				<div class="row">
-					<div class="col-md-12">
-						<h2 class="h2"><?php echo $args->name ?></h2>
-						<p class="mt-3 text-muted"><? echo _x('Please choose your dates in the availability calendar', 'renting_product_detail', 'mybooking-wp-plugin' ) ?>
-					</div>
-				</div>
-			</div>
-			<hr class="pb-3">
-      <?php mybooking_engine_get_template('mybooking-plugin-product-widget.php', array('code' => $args->code)) ?>			
-		</div>
-	</div>
+      <div class="shadow mb-5 bg-white rounded">
+          <!-- The photo -->
+          <?php if (!empty( $args->photos ) && count( $args->photos ) > 1) { ?>
+          <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+              <?php foreach( $args->photos as $key => $photo ) { ?>
+              <div class="carousel-item <?php if ($key == key($args->photos)) { ?>active<?php } ?>">
+                <img class="d-block w-100" src="<?php echo $photo->full_photo_path?>" alt="<?php echo $args->name?>">
+              </div>
+              <?php } ?>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="sr-only"><? echo _x('Previous', 'renting_product_detail', 'mybooking-wp-plugin' ) ?></span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="sr-only"><? echo _x('Next', 'renting_product_detail', 'mybooking-wp-plugin' ) ?></span>
+            </a>
+          </div>
+          <?php } else if (count($args->photos) == 1) { ?>
+            <img class="d-block product-photo" src="<?php echo $args->photos[0]->full_photo_path?>" alt="<?php echo $args->name?>">
+          <?php } else { ?>
+              <div class="text-center no-product-photo pt-3"><i class="fa fa-camera" aria-hidden="true"></i></div>
+          <?php } ?>
+          <div class="p-3">
+            <!-- Name -->
+            <h1 class="h2 mt-3"><b><?php echo $args->name ?></b></h1>
+            <!-- Short description -->
+            <?php if ( !empty( $args->short_description) ) { ?>
+              <div class="text-muted mt-1">
+                <?php echo $args->short_description ?>
+              </div>
+            <?php } ?>
+            <!-- From price -->
+            <?php if ($args->from_price > 0) { ?>
+            <h2 class="h4 mt-3 text-danger font-weight-normal"><?php printf( _x('From <b>%s</b>', 'renting_product_detail', 'mybooking-wp-plugin' ), number_format_i18n( $args->from_price ) ) ?>€</h2>
+            <?php } ?>
+            <!-- Key characteristics -->
+            <?php if ( isset( $args->key_characteristics) && !empty( $args->key_characteristics ) ) { ?>
+            <div class="card-body  key-characteristics">
+              <ul class="icon-list-items">
+                <?php if ( isset( $args->characteristic_length ) && !empty( $args->characteristic_length ) ) { ?>
+                  <li class="icon-list-item">
+                      <span class="icon-list-icon">
+                         <img src="<?php echo plugin_dir_url(__DIR__) ?>/assets/images/characteristics/length.svg"/>
+                      </span>
+                      <span class="icon-list-text text-muted"><?php printf( _x('<b>%.2f</b> m.', 'renting_product_detail', 'mybooking-wp-plugin' ), $args->characteristic_length ) ?></span>
+                  </li>
+                <?php } ?>  
+                <?php foreach ( $args->key_characteristics as $key => $value) { ?>
+                <li class="icon-list-item">
+                    <span class="icon-list-icon">
+                      <img src="<?php echo plugin_dir_url(__DIR__) ?>/assets/images/key_characteristics/<?php echo $key ?>.svg"/>
+                    </span>
+                    <span class="icon-list-text text-muted"><?php echo $value ?></span>
+                </li>
+                <?php } ?>
+              </ul>
+            </div>  
+            <?php } ?>
+            <!-- Description -->
+            <?php if ( !empty( $args->description ) ) { ?>
+              <hr>
+              <div class="mt-3">
+                <h2 class="h3 mb-3"><? echo _x('Details', 'renting_product_detail', 'mybooking-wp-plugin' ) ?></h2>
+                <?php echo $args->description ?>
+              </div>
+            <?php } ?>
+
+          </div>
+      </div>
+    </div>  
+    <div class="col-md-4">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <h2 class="h2"><b><?php echo $args->name ?></b></h2>
+            <p class="mt-3 text-muted"><? echo _x('Please choose your dates in the availability calendar', 'renting_product_detail', 'mybooking-wp-plugin' ) ?>
+          </div>
+        </div>
+      </div>
+      <hr class="pb-3">
+          <?php mybooking_engine_get_template('mybooking-plugin-product-widget.php', array('code' => $args->code)) ?>     
+    </div>
+  </div>
 </div>
 
-<?php get_footer();
+<?php get_footer(); ?>
