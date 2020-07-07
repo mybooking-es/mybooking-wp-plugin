28b7a29f5dd324d66e38845e6e147b32b235b95e
Juan Gil Miqueo <jgil@MacBook-Pro-de-Juan.local>
2020-07-08 00:33:26 +0200

    product detail

--- mybooking-templates/mybooking-plugin-reservation-tmpl.php
+++ mybooking-templates/mybooking-plugin-reservation-tmpl.php
@@ -79,44 +79,44 @@
             </div>  
             <ul class="list-group list-group-flush">
               <% for (var idx=0;idx<booking.booking_lines.length;idx++) { %>
-              <li class="list-group-item reservation-summary-card-detail">
-                 <!-- Photo -->
-                 <img class="product-img" style="width: 120px" src="<%=booking.booking_lines[idx].photo_medium%>"/>
-                 <br>
-                 <!-- Description -->
-                 <span class="product-name"><b><%=booking.booking_lines[idx].item_description_customer_translation%></b></span>
-                 <!-- Quantity -->
-                 <% if (configuration.multipleProductsSelection) { %>
-                 <span class="badge badge-info"><%=booking.booking_lines[idx].quantity%></span>
-                 <% } %>
-                 <!-- Price -->
-                 <span class="product-amount pull-right"><%=configuration.formatCurrency(booking.booking_lines[idx].item_cost)%></span>
-                 <!-- Offer/Promotion Code Appliance -->
-                 <% if (booking.booking_lines[idx].item_unit_cost_base != booking.booking_lines[idx].item_unit_cost) { %>
+                <li class="list-group-item reservation-summary-card-detail">
+                   <!-- Photo -->
+                   <img class="product-img" style="width: 120px" src="<%=booking.booking_lines[idx].photo_medium%>"/>
                    <br>
-                   <div class="pull-right">
-                     <!-- Offer -->
-                     <% if (typeof booking.booking_lines[idx].offer_name !== 'undefined' && 
-                            booking.booking_lines[idx].offer_name !== null && booking.booking_lines[idx].offer_name !== '') { %>
-                        <span class="badge badge-info"><%=booking.booking_lines[idx].offer_name%></span>
-                        <% if (booking.booking_lines[idx].offer_discount_type === 'percentage' && booking.booking_lines[idx].offer_value !== '') {%>
-                          <span class="text-danger"><%=parseInt(booking.booking_lines[idx].offer_value)%>&#37;</span>
-                        <% } %>
-                     <% } %>
-                     <!-- Promotion Code -->
-                     <% if (typeof booking.promotion_code !== 'undefined' && booking.promotion_code !== '' && 
-                            typeof booking.booking_lines[idx].promotion_code_value !== 'undefined' && booking.booking_lines.promotion_code_value !== '') { %>
-                        <span class="badge badge-success"><%=booking.promotion_code%></span>
-                        <% if (booking.booking_lines[idx].promotion_code_discount_type === 'percentage' && booking.booking_lines[idx].promotion_code !== '') {%>
-                          <span class="text-danger"><%=parseInt(booking.booking_lines[idx].promotion_code_value)%>&#37;</span>
-                        <% } %>
-                     <% } %>
-                     <span class="text-muted">
-                       <del><%=configuration.formatCurrency(booking.booking_lines[idx].item_unit_cost_base * booking.booking_lines[idx].quantity)%></del>
-                     </span>
-                   </div>   
-                 <% } %> 
-              </li>
+                   <!-- Description -->
+                   <span class="product-name"><b><%=booking.booking_lines[idx].item_description_customer_translation%></b></span>
+                   <!-- Quantity -->
+                   <% if (configuration.multipleProductsSelection) { %>
+                   <span class="badge badge-info"><%=booking.booking_lines[idx].quantity%></span>
+                   <% } %>
+                   <!-- Price -->
+                   <span class="product-amount pull-right"><%=configuration.formatCurrency(booking.booking_lines[idx].item_cost)%></span>
+                   <!-- Offer/Promotion Code Appliance -->
+                   <% if (booking.booking_lines[idx].item_unit_cost_base != booking.booking_lines[idx].item_unit_cost) { %>
+                     <br>
+                     <div class="pull-right">
+                       <!-- Offer -->
+                       <% if (typeof booking.booking_lines[idx].offer_name !== 'undefined' && 
+                              booking.booking_lines[idx].offer_name !== null && booking.booking_lines[idx].offer_name !== '') { %>
+                          <span class="badge badge-info"><%=booking.booking_lines[idx].offer_name%></span>
+                          <% if (booking.booking_lines[idx].offer_discount_type === 'percentage' && booking.booking_lines[idx].offer_value !== '') {%>
+                            <span class="text-danger"><%=parseInt(booking.booking_lines[idx].offer_value)%>&#37;</span>
+                          <% } %>
+                       <% } %>
+                       <!-- Promotion Code -->
+                       <% if (typeof booking.promotion_code !== 'undefined' && booking.promotion_code !== '' && 
+                              typeof booking.booking_lines[idx].promotion_code_value !== 'undefined' && booking.booking_lines.promotion_code_value !== '') { %>
+                          <span class="badge badge-success"><%=booking.promotion_code%></span>
+                          <% if (booking.booking_lines[idx].promotion_code_discount_type === 'percentage' && booking.booking_lines[idx].promotion_code !== '') {%>
+                            <span class="text-danger"><%=parseInt(booking.booking_lines[idx].promotion_code_value)%>&#37;</span>
+                          <% } %>
+                       <% } %>
+                       <span class="text-muted">
+                         <del><%=configuration.formatCurrency(booking.booking_lines[idx].item_unit_cost_base * booking.booking_lines[idx].quantity)%></del>
+                       </span>
+                     </div>   
+                   <% } %> 
+                </li>
               <% } %>
             </ul>
           </div>
