05a675144864eda09f0be0374e75841908b8180a
Juan Gil Miqueo <jgil@MacBook-Pro-de-Juan.local>
2021-09-17 00:27:10 +0200

    transfer summary template

---
+++ mybooking-templates/mybooking-plugin-transfer-summary.php
@@ -0,0 +1,14 @@
+<?php
+  /** 
+   * The Template for showing the renting summary step
+   *
+   * This template can be overridden by copying it to yourtheme/mybooking-templates/mybooking-plugin-summary.php
+   */
+?>
+<div class="container">
+  <div class="row">
+    <div class="col-md-8 offset-md-2">
+      <div id="mybooking_transfer_reservation_detail"></div>
+    </div>
+  </div>
+</div>
