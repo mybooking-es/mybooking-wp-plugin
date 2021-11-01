492702d434e78c4553ec098604d9933bda80b5dc
Juan Gil Miqueo <jgil@MacBook-Pro-de-Juan.local>
2021-11-01 11:35:45 +0100

    cookies compatibility with theme

--- assets/js/complements.js
+++ assets/js/complements.js
@@ -18,12 +18,12 @@ jQuery(document).ready(function () {
 	  });
   }
 
-  // Cookies  
+  // Cookies (compability with theme)
   if (jQuery("#cookie-notice").length > 0) {
-  	if (jQuery("#cookie-notice_button").length > 0) {
-  		jQuery("#cookie-notice_button").bind('click', function() {
-		  document.cookie="mb_cookieaccepted=1; expires=Thu, 18 Dec 2030 12:00:00 UTC; path=/";
-		  document.getElementById( "cookie-notice" ).style.visibility="hidden";
+  	if (jQuery("#cookie-notice_button, #mybooking-cookies_button").length > 0) {
+  		jQuery("#cookie-notice_button, #mybooking-cookies_button").bind('click', function() {
+		  	document.cookie="mb_cookieaccepted=1; expires=Thu, 18 Dec 2030 12:00:00 UTC; path=/";
+		  	document.getElementById( "cookie-notice" ).style.visibility="hidden";
   		});
   	}
 	document.cookie.indexOf( "mb_cookieaccepted" ) < 0 && ( document.getElementById( "cookie-notice" ).style.visibility="visible" );
