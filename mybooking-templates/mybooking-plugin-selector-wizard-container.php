cacb335be2afa2671138f8293958620bd1dd518a
hacheuno <hacheunomenorca@gmail.com>
2021-11-02 23:01:35 +0100

    Wizard selector

--- mybooking-templates/mybooking-plugin-selector-wizard-container.php
+++ mybooking-templates/mybooking-plugin-selector-wizard-container.php
@@ -1,20 +1,24 @@
 <?php
-  /** 
-   * The Template for showing the renting selector wizard container 
+  /**
+   * The Template for showing the renting selector wizard container
    *
    * This template can be overridden by copying it to yourtheme/mybooking-templates/mybooking-plugin-wizard-container.php
    *
    */
 ?>
-      <!-- Wizard container -->  
-      <div id="wizard_container" class="bg-white selector_wizard_container" style="display: none">
-           <!-- Title -->
-           <div id="step_title" class="text-center h5 pb-2 selector_wizard_step_title"></div>
-           <!-- Close btn -->
-           <span id="close_wizard_btn" class="selector_wizard_close_btn"><i class="fa fa-times"></i></span>
-           <!-- Container -->
-          <div id="wizard_container_step" class="p-2 selector_wizard_step_container">
-            <div id="wizard_container_step_header"></div>
-            <div id="wizard_container_step_body"></div>
-          </div>
-      </div>
+
+<!-- Wizard container -->
+<div id="wizard_container" class="selector_wizard_container" style="display: none">
+
+  <!-- Title -->
+  <div id="step_title" class="selector_wizard_step_title"></div>
+
+  <!-- Close btn -->
+  <span id="close_wizard_btn" class="selector_wizard_close_btn"><span class="dashicons dashicons-dismiss"></span></span>
+
+  <!-- Container -->
+  <div id="wizard_container_step" class="selector_wizard_step_container">
+    <div id="wizard_container_step_header"></div>
+    <div id="wizard_container_step_body"></div>
+  </div>
+</div>
--- mybooking-templates/mybooking-plugin-selector-wizard-widget.php
+++ mybooking-templates/mybooking-plugin-selector-wizard-widget.php
@@ -1,45 +1,58 @@
 <?php
-  /** 
+  /**
    * The Template for showing the renting selector widget
    *
    * This template can be overridden by copying it to yourtheme/mybooking-templates/mybooking-plugin-wizard-widget.php
    */
 ?>
-    <section class="section">
-      <!-- Search form -->
-      <div class="container">
-        <form name="wizard_search_form" class="mt-5">
-            <input type="hidden" name="pickup_place">
-            <input type="hidden" name="return_place">
-            <input type="hidden" name="date_from">
-            <input type="hidden" name="time_from">
-            <input type="hidden" name="time_to">
-            <?php if ( array_key_exists('sales_channel_code', $args) && $args['sales_channel_code'] != '' ) : ?>
-              <input type="hidden" name="sales_channel_code" value="<?php echo esc_attr( $args['sales_channel_code'] )?>" />
-            <?php endif; ?>
-            <?php if ( array_key_exists('family_id', $args) && $args['family_id'] != '' ) : ?>
-              <input type="hidden" name="family_id" value="<?php echo esc_attr(  $args['family_id'] )?>" />
-            <?php endif; ?>
-            <?php if ( array_key_exists('rental_location_code', $args) && $args['rental_location_code'] != '' ) : ?>
-              <input type="hidden" name="rental_location_code" value="<?php echo esc_attr( $args['rental_location_code'] )?>"/>
-            <?php endif; ?>            
-            <div class="row">
-              <div class="col-md-3">
-                  <label for="place_holder"><?php echo esc_html_x( 'Where?', 'renting_form_selector_wizard', 'mybooking-wp-plugin') ?></label>
-                  <input type="text" class="form-control form-control-lg bg-white" id="place_holder" 
-                         aria-describedby="pickupPlaceHolder" placeholder="<?php echo esc_attr_x( 'Select a location', 'renting_form_selector_wizard', 'mybooking-wp-plugin') ?>" readonly="true">
-              </div>
-              <div class="col-md-3">
-                  <label for="from_holder"><?php echo esc_html_x( 'When?', 'renting_form_selector_wizard', 'mybooking-wp-plugin') ?></label>
-                  <input type="text" class="form-control form-control-lg bg-white" id="from_holder" 
-                         aria-describedby="FromHolder" placeholder="<?php echo esc_attr_x( 'Select dates', 'renting_form_selector_wizard', 'mybooking-wp-plugin') ?>" readonly="true">
-              </div>    
-              <br>
-              <div class="col-md-1 d-flex align-items-end">
-                  <input id="btn_reservation" type="button" class="btn btn-success" value="<?php echo esc_attr_x( 'Book', 'renting_form_selector_wizard', 'mybooking-wp-plugin') ?>"> 
-              </div>                                     
-            </div>           
-          </div>
-        </form>
+
+<section class="mybooking mybooking-selector <?php echo esc_attr( mybooking_engine_theme_align_width() )?>">
+
+  <!-- Search form -->
+
+    <form name="wizard_search_form" class="mybooking-selector_form">
+      <div class="mybooking-selector_group mb-inline">
+
+        <!-- Pickup -->
+        <div class="mybooking-selector_wizard-field">
+          <i class="mybooking-selector_field-icon">
+						<span class="dashicons dashicons-location"></span>
+					</i>
+          <label for="place_holder">
+            <?php echo esc_html_x( 'Where?', 'renting_form_selector_wizard', 'mybooking-wp-plugin') ?>
+          </label>
+          <input type="text" class="mb-form-control" id="place_holder" aria-describedby="pickupPlaceHolder" placeholder="<?php echo esc_attr_x( 'Select a location', 'renting_form_selector_wizard', 'mybooking-wp-plugin') ?>" readonly="true">
+        </div>
+
+        <!-- Date -->
+        <div class="mybooking-selector_wizard-field">
+          <i class="mybooking-selector_field-icon">
+						<span class="dashicons dashicons-calendar-alt"></span>
+					</i>
+          <label for="from_holder">
+            <?php echo esc_html_x( 'When?', 'renting_form_selector_wizard', 'mybooking-wp-plugin') ?>
+          </label>
+          <input type="text" class="mb-form-control" id="from_holder" aria-describedby="FromHolder" placeholder="<?php echo esc_attr_x( 'Select dates', 'renting_form_selector_wizard', 'mybooking-wp-plugin') ?>" readonly="true">
+        </div>
+        <input  class="mb-button mybooking-selector_button" id="btn_reservation" type="submit" value="<?php echo esc_attr_x( 'Book', 'renting_form_selector_wizard', 'mybooking-wp-plugin') ?>">
       </div>
-    </section>
+
+      <input type="hidden" name="pickup_place">
+      <input type="hidden" name="return_place">
+      <input type="hidden" name="date_from">
+      <input type="hidden" name="time_from">
+      <input type="hidden" name="time_to">
+
+      <?php if ( array_key_exists('sales_channel_code', $args) && $args['sales_channel_code'] != '' ) : ?>
+        <input type="hidden" name="sales_channel_code" value="<?php echo esc_attr( $args['sales_channel_code'] )?>" />
+      <?php endif; ?>
+
+      <?php if ( array_key_exists('family_id', $args) && $args['family_id'] != '' ) : ?>
+        <input type="hidden" name="family_id" value="<?php echo esc_attr(  $args['family_id'] )?>" />
+      <?php endif; ?>
+
+      <?php if ( array_key_exists('rental_location_code', $args) && $args['rental_location_code'] != '' ) : ?>
+        <input type="hidden" name="rental_location_code" value="<?php echo esc_attr( $args['rental_location_code'] )?>"/>
+      <?php endif; ?>
+    </form>
+</section>
