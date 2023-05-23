3555d131459c558123046c8485f015b8c63fcaaa
Anabel Muñoz <amvisualdesigner@gmail.com>
2023-05-23 15:13:24 +0200

    Shift picker submit button corrections

--- mybooking-templates/mybooking-plugin-shift-picker.php
+++ mybooking-templates/mybooking-plugin-shift-picker.php
@@ -12,7 +12,11 @@
 ?>
 <!-- ShiftPicker start -->
 <div id="shift-picker-<?php echo isset($args['shift_picker_id']) ? esc_attr( $args['shift_picker_id'] ) : '' ?>" data-category-code="<?php echo isset($args['category_code']) ? esc_attr( $args['category_code'] ) : '' ?>"  data-sales-channel-code="<?php echo isset($args['sales_channel_code']) ? esc_attr( $args['sales_channel_code'] ) : '' ?>" data-rental-location-code="<?php echo isset($args['rental_location_code']) ? esc_attr( $args['rental_location_code'] ) : '' ?>" class="mybooking-rent-shift-picker-content">
-		<form name="mybooking-rent-shift-picker-form" method="POST">
+		<form
+			name="mybooking-rent-shift-picker-form"
+			method="get"
+			enctype="application/x-www-form-urlencoded"
+			class="mybooking-form">
 			<div class="mybooking-rent-shift-picker-head">
 				<h2 class="mb-sidebar_title">
 					<?php echo esc_html_x( 'Select a shift', 'renting_shift_picker', 'mybooking-wp-plugin') ?>
