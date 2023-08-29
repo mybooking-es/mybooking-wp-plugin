86c21efd2dc97a9c885009c882400045668b914f
Juan Gil Miqueo <jgil@MacBook-Pro-de-Juan.local>
2023-08-29 11:05:34 +0200

    create activity pages

---
+++ includes/settings/api/class-mybooking-create-activity-pages.php
@@ -0,0 +1,103 @@
+<?php
+ 
+  class MybookingCreateActivityPages extends MybookingCreatePages {
+  	
+
+    /**
+    * Create renting pages
+    */
+    function createRentingPages($navigation) {
+      
+      // Created pages (posts) ids
+      $pages = array();
+
+      // Define pages
+      $pages_to_create = array(
+        'checkout'           => array(
+           'option' => 'mybooking_plugin_settings_checkout_page',
+           'title' => _x( 'Check-out', 'plugin_custom_renting_pages', 'mybooking-wp-plugin' ),
+           'content' => '<!-- wp:shortcode -->[mybooking_rent_engine_complete]<!-- /wp:shortcode -->',
+           'slug' => 'checkout',
+           'order' => 3,
+        ),
+        'summary'           => array(
+           'option' => 'mybooking_plugin_settings_summary_page',
+           'title' => _x( 'Summary', 'plugin_custom_renting_pages', 'mybooking-wp-plugin' ),
+           'content' => '<!-- wp:shortcode -->[mybooking_rent_engine_summary]<!-- /wp:shortcode -->',
+           'slug' => 'summary',
+           'order' => 4,
+        ),
+        'my_reservation'           => array(
+           'option' => 'mybooking_plugin_settings_my_reservation_page',
+           'title' => _x( 'My reservation', 'plugin_custom_renting_pages', 'mybooking-wp-plugin' ),
+           'content' => '<!-- wp:shortcode -->[mybooking_rent_engine_reservation]<!-- /wp:shortcode -->',
+           'slug' => 'my-reservation',
+           'order' => 5,
+        )
+      );        
+
+      if ( $navigation == 'selector' ) {
+
+        // Home with selector
+        $pages_to_create['home_test'] = array( 
+                                               'option' => 'mybooking_plugin_settings_home_test_page',
+                                               'title' => _x( 'Home Test', 'plugin_custom_renting_pages', 'mybooking-wp-plugin' ),
+                                               'content' => '<!-- wp:shortcode -->[mybooking_rent_engine_selector]<!-- /wp:shortcode -->',
+                                               'slug' => 'home-test',
+                                               'order' => 1,
+                                              );
+        // Choose product (search result)           
+        $pages_to_create['choose_products'] = array( 
+                                               'option' => 'mybooking_plugin_settings_choose_products_page',
+                                               'title' => _x( 'Search result', 'plugin_custom_renting_pages', 'mybooking-wp-plugin' ),
+                                               'content' => '<!-- wp:shortcode -->[mybooking_rent_engine_product_listing]<!-- /wp:shortcode -->',
+                                               'slug' => 'search-result',
+                                               'order' => 2,
+                                              );
+      }
+
+      // Set the options settings (TODO Does not exist)
+      $settings_activities = (array) get_option("mybooking_plugin_settings_activities");
+
+      // Create pages
+      foreach( $pages_to_create as $key => $page_to_create ) {
+
+        $post_id = $this->createPage($page_to_create['title'],
+                                     $page_to_create['content'],
+                                     $page_to_create['slug'],
+                                     $page_to_create['order']);
+      
+
+        if ($post_id) {
+          // Update settings page
+          $settings_activities[$page_to_create['option']] = $post_id;
+
+          array_push($pages, 
+            array( 'id' => $post_id,
+                   'title' => $page_to_create['title'],
+                   'permalink' => get_permalink( $post_id ),
+                   'edit_post_link' => get_edit_post_link ( $post_id )));
+
+        }
+
+      }
+
+      // Update activities settings (activities process pages)
+      update_option( "mybooking_plugin_settings_activities", $settings_activities );
+
+      if (count($pages) > 0) {
+        
+        $pages_info = array(
+          "navigation" => $navigation,
+          "pages" => $pages
+        );
+
+        return $pages_info;
+
+      } else {
+        return null;
+      }
+    
+    }
+
+  }
