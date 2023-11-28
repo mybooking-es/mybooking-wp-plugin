97c5b33331bbac0a0527596da8039f382908f057
Yurak <info@yuraksisa.com>
2023-11-28 11:58:26 +0100

    retro compatiblity integration tab

--- mybooking-wp-plugin.php
+++ mybooking-wp-plugin.php
@@ -3,7 +3,7 @@
 * Plugin Name: MyBooking Reservation Engine
 * Plugin URI: https://www.mybooking.es/
 * Description: Reservation Engine WordPress frontend for Mybooking. Transforms wordpress into a rental / accommodation / tours reservation engine.
-* Version: 1.10.4.2
+* Version: 1.10.4.3
 * Author: mybooking
 * Author URI: https://mybooking.es/
 **/
