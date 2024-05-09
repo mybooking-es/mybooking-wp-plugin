82947fa56a5c8f1da5a660ac8a46946d971f4319
Anabel Muñoz <anabel@mybooking.es>
2024-05-09 18:34:30 +0200

    Added step component in view, pending conditionals

--- mybooking-templates/mybooking-plugin-reservation-tmpl.php
+++ mybooking-templates/mybooking-plugin-reservation-tmpl.php
@@ -467,8 +467,23 @@
 <script type="text/tmpl" id="script_reservation_form">
   <% if (configuration.rentingFormFillDataAddress || configuration.rentingFormFillDataDriverDetail || configuration.rentingFormFillDataNamedResources) { %>
     <form class="mybooking-form" id="form-reservation" name="booking_information_form" autocomplete="off">
-      <div class="mb-alert light">
-        <?php echo esc_html_x( 'Please complete the information to speed up the delivery process on the scheduled date', 'renting_my_reservation', 'mybooking-wp-plugin') ?>
+      <div class="mb-alert light text-left">
+        <h4 class="mb--txt-fw_light"><?php echo esc_html_x( 'Please follow the steps and complete the booking process.', 'renting_my_reservation', 'mybooking-wp-plugin') ?></h4>
+        <h6 class="mb--txt-fw_light"><?php echo esc_html_x( 'This way we will have everything ready for the scheduled date.', 'renting_my_reservation', 'mybooking-wp-plugin') ?></h6>
+        <div class="mb--steps-wrapper">
+          <div class="mb--step mb--active">
+            <span class="mb--step-number">1</span>
+            <span class="mb--step-text"><?php echo esc_html_x( 'Complete personal data', 'renting_my_reservation', 'mybooking-wp-plugin') ?></span>
+          </div>
+          <div class="mb--step mb--disabled">
+            <span class="mb--step-number">2</span>
+            <span class="mb--step-text"><?php echo esc_html_x( 'Upload documentation', 'renting_my_reservation', 'mybooking-wp-plugin') ?></span>
+          </div>
+          <div class="mb--step mb--disabled">
+            <span class="mb--step-number">3</span>
+            <span class="mb--step-text"><?php echo esc_html_x( 'Firm contract', 'renting_my_reservation', 'mybooking-wp-plugin') ?></span>
+          </div>
+        </div>
       </div>
 
       <!-- // Customer  -->
