ef126d587da8b719771094bc4a67b8e92db56514
Anabel Muñoz <anabel@mybooking.es>
2024-05-30 15:19:54 +0200

    Driver is customer alert added

--- mybooking-templates/mybooking-plugin-reservation-form-tmpl.php
+++ mybooking-templates/mybooking-plugin-reservation-form-tmpl.php
@@ -28,13 +28,22 @@
     <!-- Driver is customer check -->
     <div class="mb-section mb-panel-container">
       <% if (configuration.rentingFormFillDataDriverDetail && booking.driver_type == 'driver' && !booking.has_optional_external_driver && booking.customer_type != 'legal_entity') { %>
-        <div class="mb-form-row">
-          <label>
-            <input type="checkbox" name="driver_is_customer" id="driver_is_customer" <% if (booking.driver_is_customer != false) { %>checked<% } %> <% if (!booking.can_edit_online){%>disabled<%}%>>
-            &nbsp;
-            <?php echo esc_html_x('The customer is the driver', 'renting_my_reservation', 'mybooking-wp-plugin') ?>
-          </label>
+        <div class="mb-alert warning text-left">
+          <?php echo esc_html_x('The customer represents the contract holder. Typically, the driver is the contract holder.', 'renting_my_reservation', 'mybooking-wp-plugin') ?>
+          <strong>
+            <?php echo esc_html_x("Assign a different customer from the driver only if you want the contract to be under another person's name.", 'renting_my_reservation', 'mybooking-wp-plugin') ?>
+          </strong>
+          <?php echo esc_html_x('This does not affect billing, as you can always request the invoice with different tax details than those on the contract.', 'renting_my_reservation', 'mybooking-wp-plugin') ?>
+          <br /><br />
+          <div class="mb-form-row">
+            <label>
+              <input type="checkbox" name="driver_is_customer" id="driver_is_customer" <% if (booking.driver_is_customer != false) { %>checked<% } %> <% if (!booking.can_edit_online){%>disabled<%}%>>
+              &nbsp;
+              <?php echo esc_html_x('The customer is the driver', 'renting_my_reservation', 'mybooking-wp-plugin') ?>
+            </label>
+          </div>
         </div>
+        
         <br />
       <% } %>
 
