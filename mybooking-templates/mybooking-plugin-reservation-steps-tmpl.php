684fa0f567d7eb76b997d236169503d7d0abdc5d
Anabel Muñoz <anabel@mybooking.es>
2024-06-04 12:17:40 +0200

    Moved pay now button after steps and others styles changes

--- mybooking-templates/mybooking-plugin-reservation-steps-tmpl.php
+++ mybooking-templates/mybooking-plugin-reservation-steps-tmpl.php
@@ -22,32 +22,35 @@
           <ul class="mb--steps-wrapper">
             <li class="mb--step <% if (!booking.required_data_completed) { %>mb--active<% } else { %>mb--completed<% } %>">
               <a href="#reservation_customer_container">
-                <span class="mb--step-number">1</span>
+              <% if (booking.engine_sign_contract) { %><span class="mb--step-number">1</span><% } %>
                 <span class="mb--step-text"><?php echo esc_html_x( 'Complete data', 'renting_my_reservation', 'mybooking-wp-plugin') ?></span>
               </a>
             </li>
             <% if (booking.engine_sign_contract) { %>   
-            <li class="mb--step <% if (booking.required_data_completed && !booking.customer_documents_uploaded) { %>mb--active<% } else if (booking.customer_documents_uploaded) { %>mb--completed<% } else { %>mb--disabled<% } %>">
-              <a href="#documents_upload_container">
-                <span class="mb--step-number">2</span>
-                <span class="mb--step-text"><?php echo esc_html_x( 'Upload documentation', 'renting_my_reservation', 'mybooking-wp-plugin') ?></span>
-              </a>
-            </li>
-            <li class="mb--step <% if (booking.required_data_completed && booking.customer_documents_uploaded && !booking.contract_signed) { %>mb--active<% } else if (booking.contract_signed) { %>mb--completed<% } else { %>mb--disabled<% } %>">
-              <a href="#contract_signature_container">
-                <span class="mb--step-number">3</span>
-                <span class="mb--step-text"><?php echo esc_html_x( 'Firm contract', 'renting_my_reservation', 'mybooking-wp-plugin') ?></span>
-              </a>
-            </li>        
+              <li class="mb--step <% if (booking.required_data_completed && !booking.customer_documents_uploaded) { %>mb--active<% } else if (booking.customer_documents_uploaded) { %>mb--completed<% } else { %>mb--disabled<% } %>">
+                <a href="#documents_upload_container">
+                  <span class="mb--step-number">2</span>
+                  <span class="mb--step-text"><?php echo esc_html_x( 'Upload documentation', 'renting_my_reservation', 'mybooking-wp-plugin') ?></span>
+                </a>
+              </li>
+              <li class="mb--step <% if (booking.required_data_completed && booking.customer_documents_uploaded && !booking.contract_signed) { %>mb--active<% } else if (booking.contract_signed) { %>mb--completed<% } else { %>mb--disabled<% } %>">
+                <a href="#contract_signature_container">
+                  <span class="mb--step-number">3</span>
+                  <span class="mb--step-text"><?php echo esc_html_x( 'Firm contract', 'renting_my_reservation', 'mybooking-wp-plugin') ?></span>
+                </a>
+              </li>
+            <% } %>
+            <% if (sales_process && sales_process.can_pay) { %>
+              <li class="mb--step">
+                <a href="#payment_view"  id="btn_payment_detail">
+                  <span class="mb--step-number"><span class="dashicons dashicons-money"></span></span>
+                  <span class="mb--step-text">
+                    <?php echo esc_html_x( 'Pay now!', 'renting_my_reservation', 'mybooking-wp-plugin') ?>
+                  </span>
+                </a>
+              </li>
             <% } %>
           </ul>
         </div>
-      <% if (sales_process && sales_process.can_pay) { %>
-        <div class="mb-col-md-6 mb-col-lg-4 mb--flex-align_end">
-            <button  id="btn_payment_detail" class="mb-button mb-accent-color mb-width-100">
-              <?php echo esc_html_x( 'Pay now!', 'renting_my_reservation', 'mybooking-wp-plugin') ?>
-            </button>
-        </div>
-      <% } %>
     </div>
 </script>
