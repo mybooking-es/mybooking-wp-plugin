b0ec386162ee5292146d462a6d96b13a246f8298
Juan Miqueo <jgil@MacBook-Air-de-Juan.local>
2024-06-24 17:46:24 +0200

    tab documentation disabled

--- mybooking-templates/mybooking-plugin-reservation-steps-tmpl.php
+++ mybooking-templates/mybooking-plugin-reservation-steps-tmpl.php
@@ -30,13 +30,13 @@
               </a>
             </li>
             <% if (booking.engine_sign_contract) { %>   
-              <li class="mb--step <% if (booking.required_data_completed && !booking.customer_documents_uploaded) { %>mb--active<% } else if (booking.customer_documents_uploaded) { %>mb--completed<% } else { %>mb--disabled<% } %>">
+              <li class="mb--step <% if (booking.can_edit_online && booking.required_data_completed && !booking.customer_documents_uploaded) { %>mb--active<% } else if (booking.customer_documents_uploaded) { %>mb--completed<% } else { %>mb--disabled<% } %>">
                 <a href="#documents_upload_container">
                   <span class="mb--step-number">2</span>
                   <span class="mb--step-text"><?php echo esc_html_x( 'Upload documentation', 'renting_my_reservation', 'mybooking-wp-plugin') ?></span>
                 </a>
               </li>
-              <li class="mb--step <% if (booking.required_data_completed && booking.customer_documents_uploaded && !booking.contract_signed) { %>mb--active<% } else if (booking.contract_signed) { %>mb--completed<% } else { %>mb--disabled<% } %>">
+              <li class="mb--step <% if (booking.can_edit_online && booking.required_data_completed && booking.customer_documents_uploaded && !booking.contract_signed) { %>mb--active<% } else if (booking.contract_signed) { %>mb--completed<% } else { %>mb--disabled<% } %>">
                 <a href="#contract_signature_container">
                   <span class="mb--step-number">3</span>
                   <span class="mb--step-text"><?php echo esc_html_x( 'Firm contract', 'renting_my_reservation', 'mybooking-wp-plugin') ?></span>
