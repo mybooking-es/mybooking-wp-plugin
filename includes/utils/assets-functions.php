ea44668db53bc43e32ef685ab067916ba2e2845a
Juan Miqueo <jgil@MacBook-Air-de-Juan.local>
2025-05-16 12:01:48 +0200

    rename filter

--- includes/utils/assets-functions.php
+++ includes/utils/assets-functions.php
@@ -5,5 +5,5 @@
    */
 function mybooking_engine_get_characteristics_icons_url() {
   $default_url = plugin_dir_url( dirname( __DIR__ ) ) . 'assets/images/key_characteristics/';
-  return apply_filters( 'mybooking_characteristics_icons_url', $default_url );
+  return apply_filters( 'mybooking_engine_characteristics_icons_url', $default_url );
 }
